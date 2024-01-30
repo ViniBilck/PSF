d66ab5ee5cae707933035125f908de350b670566
vinibilck <bilck.vinicius1998@gmail.com>
2024-01-30 18:16:27 -0300

    init & get_coordinates

--- psf/psf.py
+++ psf/psf.py
@@ -1,21 +1,24 @@
+from typing import List
+
 from astropy.modeling import models, fitting
 from astropy.io import fits
 import numpy as np
 
 
 class PSF(object):
-    def __init__(self, galaxy_file: str, stars_coordinates: str, delta_axes: float = 20.0):
-        self.stars_coordinates_file = stars_coordinates
+    def __init__(self, galaxy_file: str, stars_coordinates_file: str, delta_axes: float = 20.0):
         self.delta_axes = delta_axes
-        self.galaxy_file = galaxy_file
+        self.galaxy_file_data = fits.getdata(galaxy_file)
+        self.stars_coordinates = self.get_stars_coordinates(stars_coordinates_file)
 
-    def get_stars_coordinates(self):
-        with open(self.stars_coordinates_file) as coordinate_list:
-            text_read = coordinate_list.read()
-        return text_read
+    def get_stars_coordinates(self, star_coordinates_file: str) -> list[list[str]]:
+        with open(star_coordinates_file) as coordinate_list:
+            coordinates = [line.strip() for line in coordinate_list]
+            points = [point.split(",") for point in coordinates]
+        return points
 
     def get_stars(self):
-        return fits.getdata(self.galaxy_file)
+        return 0
 
     def do_cuts(self):
         return 0
